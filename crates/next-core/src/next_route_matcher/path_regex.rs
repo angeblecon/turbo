3707b08d9c4a25d615c911276b2f84bd257ff93e
Justin Ridgewell <justin@ridgewell.name>
2023-02-15 15:58:48 -0500

    Clippy

--- crates/next-core/src/next_route_matcher/path_regex.rs
+++ crates/next-core/src/next_route_matcher/path_regex.rs
@@ -48,7 +48,7 @@ impl RouteMatcher for PathRegex {
                             NamedParamKind::Multi => Param::Multi(
                                 value
                                     .as_str()
-                                    .split("/")
+                                    .split('/')
                                     .map(|segment| segment.to_string())
                                     .collect(),
                             ),
